d59da568b072cbba8763f68d5614e00c8761d9a9
MaxWong03 <madmaxmmajournal@gmail.com>
2019-10-23 13:34:23 -0700

    add extra tab to test fb sign up / login

---
+++ screens/facebookScreen.js
@@ -0,0 +1,22 @@
+import * as Facebook from 'expo-facebook';
+import * as WebBrowser from 'expo-web-browser';
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+import { MonoText } from '../components/StyledText';
+
+export default function fbScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Details!</Text>
+    </View>
+  );
+}
