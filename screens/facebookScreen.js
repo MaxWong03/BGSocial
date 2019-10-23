5102ecea8f5ebd5f85413f47d7ad24f0610ecc29
MaxWong03 <madmaxmmajournal@gmail.com>
2019-10-23 13:44:56 -0700

    add fb screen for testing fb login

--- screens/facebookScreen.js
+++ screens/facebookScreen.js
@@ -16,7 +16,30 @@ import { MonoText } from '../components/StyledText';
 export default function fbScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Details!</Text>
+      <Text>Details</Text>
     </View>
   );
+}
+
+async function logIn() {
+  try {
+    const {
+      type,
+      token,
+      expires,
+      permissions,
+      declinedPermissions,
+    } = await Facebook.logInWithReadPermissionsAsync('<APP_ID>', {
+      permissions: ['public_profile'],
+    });
+    if (type === 'success') {
+      // Get the user's name using Facebook's Graph API
+      const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
+      Alert.alert('Logged in!', `Hi ${(await response.json()).name}!`);
+    } else {
+      // type === 'cancel'
+    }
+  } catch ({ message }) {
+    alert(`Facebook Login Error: ${message}`);
+  }
 }
