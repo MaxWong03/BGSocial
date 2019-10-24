12bcc0b2045ab316a1d13792a53a4d640ad0f0af
MaxWong03 <madmaxmmajournal@gmail.com>
2019-10-23 21:28:06 -0700

    basic login through facebook complete

--- screens/facebookScreen.js
+++ screens/facebookScreen.js
@@ -16,12 +16,12 @@ async function loginWithFacebook() {
       permissions,
       declinedPermissions,
     } = await Facebook.logInWithReadPermissionsAsync('561305434657781', {
-      permissions: ['public_profile'],
+      permissions: ['public_profile'], //permission email doesnt give you email, we have to use token / id to autehnticate ?
     });
     if (type === 'success') {
       // Get the user's name using Facebook's Graph API
       const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
-      Alert.alert('Logged in!', `Hi ${(await response.json()).name}!`);
+      Alert.alert(`User: ${(await response.json()).name}`);
     } else {
       // type === 'cancel'
     }
@@ -32,10 +32,14 @@ async function loginWithFacebook() {
 
 export default function fbScreen() {
   const { navigate } = useNavigation();
+  const loginAndRender = () => {
+    loginWithFacebook()
+      .then(() => navigate('Main'));
+  }
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Button
-        onPress={() => loginWithFacebook()}
+        onPress={() => loginAndRender()}
         title="Login with facebook"
       >
       </Button>
