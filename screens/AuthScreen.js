c65a5087094cfdee51a86d65b96ad3c7181f87dc
MaxWong03 <madmaxmmajournal@gmail.com>
2019-10-24 09:58:35 -0700

    change LoginRegisterScreen.js to AuthScreen.js that will contain Login / Register Component

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -4,7 +4,7 @@ import {
 } from 'react-native';
 import FBAuth from '../components/FBAuth'
 
-export default function LoginRegisterScreen() {
+export default function AuthScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <FBAuth title={"Login With Facebook"}/>
