a126f87771824f5c58680ce4c31de93209757f57
zongxili <zongxiliwork@gmail.com>
2019-10-26 12:52:15 -0700

    added create screene

---
+++ screens/CreateEvent.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+import {
+  // StyleSheet,
+  Text,
+  View,
+  // Dimensions
+} from 'react-native';
+
+import { ListItem } from 'react-native-elements'
+
+
+export default function createEventScreen() {
+  return (
+    <View> 
+      <Text>Need to import button options here</Text>
+
+      <ListItem/>
+
+    </View>
+  );
+}
