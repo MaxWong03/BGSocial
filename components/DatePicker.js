0be705b2e5c3a6702d6763407b5e99a853713092
MaxWong03 <madmaxmmajournal@gmail.com>
2019-10-28 13:37:05 -0700

    convert datepicker to functional component

--- components/DatePicker.js
+++ components/DatePicker.js
@@ -1,38 +1,67 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Button, View } from "react-native";
 import DateTimePicker from "react-native-modal-datetime-picker";
- 
-export default class DateTimePickerTester extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isDateTimePickerVisible: false
-    };
+
+export default function DatePicker() {
+  const [visible, setVisible] = useState(false);
+
+  const showDatePicker = () => {
+    setVisible(true);
   }
- 
-  showDateTimePicker = () => {
-    this.setState({ isDateTimePickerVisible: true });
-  };
- 
-  hideDateTimePicker = () => {
-    this.setState({ isDateTimePickerVisible: false });
-  };
- 
-  handleDatePicked = date => {
+
+
+  const closeDatePicker = () => {
+    setVisible(false);
+  
+  }
+  const getPickedDate = (date) => {
     console.log("A date has been picked: ", date);
-    this.hideDateTimePicker();
-  };
- 
-  render() {
-    return (
-      <>
-        <Button title="Show DatePicker" onPress={this.showDateTimePicker} />
-        <DateTimePicker
-          isVisible={this.state.isDateTimePickerVisible}
-          onConfirm={this.handleDatePicked}
-          onCancel={this.hideDateTimePicker}
-        />
-      </>
-    );
+    closeDatePicker();
   }
+
+  return (
+    <>
+      <Button title="Show DatePicker" onPress={showDatePicker} />
+      <DateTimePicker
+        isVisible={visible}
+        onConfirm={getPickedDate}
+        onCancel={closeDatePicker}
+      />
+    </>
+  );
 }
+
+// export default class DateTimePickerTester extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       isDateTimePickerVisible: false
+//     };
+//   }
+
+//   showDateTimePicker = () => {
+//     this.setState({ isDateTimePickerVisible: true });
+//   };
+
+//   hideDateTimePicker = () => {
+//     this.setState({ isDateTimePickerVisible: false });
+//   };
+
+//   handleDatePicked = date => {
+//     console.log("A date has been picked: ", date);
+//     this.hideDateTimePicker();
+//   };
+
+//   render() {
+//     return (
+//       <>
+//         <Button title="Show DatePicker" onPress={this.showDateTimePicker} />
+//         <DateTimePicker
+//           isVisible={this.state.isDateTimePickerVisible}
+//           onConfirm={this.handleDatePicked}
+//           onCancel={this.hideDateTimePicker}
+//         />
+//       </>
+//     );
+//   }
+// }
