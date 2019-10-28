5c8f9b72cfe50e84ef5859cc0edaca02ce2e9091
MaxWong03 <madmaxmmajournal@gmail.com>
2019-10-27 20:03:01 -0700

    sync react-dom and react-test-render version to perform test

--- components/__tests__/InviteFriend.test.js
+++ components/__tests__/InviteFriend.test.js
@@ -1,11 +1,47 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import ReactDOM from "react-dom";
 
-import InviteFriendsModal from '../InviteFriendsModal';
 
-describe('<InviteFriendsModal />', () => {
-  it('has 1 child', () => {
-    const tree = renderer.create(<App />).toJSON();
-    expect(tree.children.length).toBe(1);
+import InviteFriendsOverlay from '../InviteFriendsOverlay';
+
+describe('<InviteFriendsOverlay />', () => {
+  console.log('123');
+  it("renders without crashing", () => {
+    const Overlay = renderer.create(<InviteFriendsOverlay/>).toJSON();
+    expect(Overlay).toMatchSnapshot();
   });
 });
+
+const friendsArray = [
+  {
+    "friend_id": 1,
+    "friend_name": "Max Wong",
+    "friend_avatar": "https://platform-lookaside.fbsbx.com/platform/profilepic/?asid=921623601546635&height=350&width=350&ext=1574626882&hash=AeSZ-ILZTTrn2hrH",
+    "invited": true
+  },
+  {
+    "friend_id": 2,
+    "friend_name": "Zongxi Li",
+    "friend_avatar": "https://platform-lookaside.fbsbx.com/platform/profilepic/?asid=2184479695188878&height=350&width=350&ext=1574626952&hash=AeS_rrrvqq3FXxd6",
+    "invited": false
+  },
+  {
+    "friend_id": 4,
+    "friend_name": "Jaleel Will",
+    "friend_avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/abelcabans/128.jpg",
+    "invited": true
+  },
+  {
+    "friend_id": 5,
+    "friend_name": "Allen Olson",
+    "friend_avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/BenouarradeM/128.jpg",
+    "invited": true
+  },
+  {
+    "friend_id": 6,
+    "friend_name": "Mr. Clay Nolan",
+    "friend_avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/josemarques/128.jpg",
+    "invited": false
+  }
+]
