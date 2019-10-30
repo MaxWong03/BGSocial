c73669853309aca27605267e2e6146bae92099c2
Camila Rivera <camila.riverart@gmail.com>
2019-10-30 00:38:28 -0700

    Set authorization token facebook

--- components/FBAuth.js
+++ components/FBAuth.js
@@ -6,6 +6,8 @@ import { NavigationActions } from 'react-navigation';
 import { API_HOST } from './../settings/app.config';
 import axios from 'axios';
 
+import api from './../api';
+
 import { getUserInfoContext } from './../hooks/sessionContext';
 
 async function loginWithFacebook() {
@@ -25,9 +27,15 @@ async function loginWithFacebook() {
       const { name, id: fbID } = basicInfo.data;
       const profileResponse = await axios.get(`https://graph.facebook.com/v4.0/${fbID}/picture?height=350&width=350`)
       const { responseURL: profilePicture } = profileResponse.request;
-      const { data: userData } = await axios.get(`${API_HOST}/users/facebook/${fbID}`);
+      const response = await axios.post(`${API_HOST}/facebook-login/${fbID}`);
 
-      return { type, fbID, profilePicture, name, userData }
+      const authorizationToken = response.headers['x-auth-token'];
+      console.log('authorizationToken', authorizationToken);
+
+      console.log(api);
+      api.defaults.headers.common['x-auth-token'] = authorizationToken;
+
+      return { type, fbID, profilePicture, name, userData: response.data }
     } else { //type === 'cancel', user doesn't wanna login
       return { type };
     }
