a7d1858124c6ef9f03c34ca5fbd5779770d3c568
zongxili <zongxiliwork@gmail.com>
2019-10-31 21:44:43 -0700

    modified const formatDateWithTime = function (date) {

--- utils/index.js
+++ utils/index.js
@@ -1,11 +1,7 @@
 import moment from 'moment';
 
-// const formatDateWithTime = function (date) {
-//   return moment(date).format('ddd, MMM DD [at] hh:mm A');
-// }
-
 const formatDateWithTime = function (date) {
-  return  moment(date).format('ddd, MMM DD [at] hh:mm A');
+  return moment(date).format('ddd, MMM DD [at] hh:mm A');
 }
 
 const arrayToObject = function (objectsArray, key) { // key === 'id'
