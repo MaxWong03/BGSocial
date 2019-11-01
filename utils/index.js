c66f5a454fd7437540183d1ae63e30f2a66bbbd8
zongxili <zongxiliwork@gmail.com>
2019-10-31 21:43:35 -0700

    modified the formatDateWithTime

--- utils/index.js
+++ utils/index.js
@@ -5,11 +5,7 @@ import moment from 'moment';
 // }
 
 const formatDateWithTime = function (date) {
-  // const formatDate = moment(date).format('ddd, MMM DD [at] hh:mm A');
-  if (date === null) return "New game"
-  else{
-    return moment(date).format('ddd, MMM DD [at] hh:mm A');
-  }
+  return  moment(date).format('ddd, MMM DD [at] hh:mm A');
 }
 
 const arrayToObject = function (objectsArray, key) { // key === 'id'
