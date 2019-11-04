fe16ccfee9a26229f87cc272083eb49421137a88
MaxWong03 <madmaxmmajournal@gmail.com>
2019-11-04 13:29:54 -0800

    build template post request to db

--- screens/CreatePlayScreen.js
+++ screens/CreatePlayScreen.js
@@ -9,10 +9,13 @@ import RecordGame from '../components/RecordGame';
 import RecordTime from '../components/RecordTime';
 import RecordDuration from '../components/RecordDuration';
 import useScore from '../hooks/useScore';
+import { api } from './../api';
+import { useNavigation } from 'react-navigation-hooks';
 
 export default function CreatePlayScreen() {
   const userFriends = useNavigationParam('userFriends');
   const userGames = useNavigationParam('userGames');
+  const { navigate } = useNavigation();
   const { friendSlots, changeFriendSlot } = useFriendSlot();
   const { userData } = getUserInfo();
   const { avatar, id, name } = userData;
@@ -31,7 +34,30 @@ export default function CreatePlayScreen() {
     console.log('gameRecord:', gameRecord);
     console.log('duration:', duration);
     console.log('winnerList:', getWinners());
-
+    const newPlay = {
+      date,
+      duration: "05:10:00",
+      game_id: gameRecord[0],
+      playUsers: scoreList.map(scoreObj => {
+        return {
+          score: scoreObj.score,
+          user_id: scoreObj.id,
+          is_winner: getWinners().includes(scoreObj.id)
+        }
+      })
+    }
+    api.post(`/plays/`, {
+      "date": "2019-09-11T00:00:00.000Z",
+      "duration": "05:10:00",
+      "game_id": 4,
+      "event_id": null,
+      "playsUsers": [{
+        "score": 56,
+        "is_winner": false,
+        "user_id": 1
+      }]
+    })
+      .then(() => navigate('Plays'))
   }
 
   return (
