ca69031dd2e564666672c856cd929ce3d4bcf24e
zongxili <zongxiliwork@gmail.com>
2019-11-04 07:55:03 -0800

    set time out attempt

--- screens/FriendsScreen.js
+++ screens/FriendsScreen.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
-import { Header, Avatar, Button, Icon } from "react-native-elements";
+import { Header, Avatar, Button, Icon, ButtonGroup } from "react-native-elements";
 import { api } from '../api';
 import useFriendsData from '../hooks/useFriendsData';
 import { getUserInfo } from './../hooks/sessionContext';
@@ -22,6 +22,7 @@ export default function FriendsScreen({ navigation }) {
       setAllUsers(res.data.users);
     });
   }, []);
+
 
   const { state: friendsList, dispatchFriends, ADD_FRIEND, REMOVE_FRIEND } = useFriendsData();
 
@@ -59,7 +60,56 @@ export default function FriendsScreen({ navigation }) {
   };
 
 
-  // 
+  // button group attempt
+  const [screenState, setButtonGroup] = useState(0);
+
+  const slider = function (currentScreen) {
+    setButtonGroup(currentScreen);
+  };
+
+  const buttons = ['My friend', 'Received Request'];
+
+  let listToShow = [];
+
+  // if (screenState === 0) {
+  //   listToShow = confirmEvents(state, userId);
+  // }
+  // else if (screenState == 1) {
+  //   listToShow = pendingEvents(state, userId);
+  // }
+
+
+  // infinite refreshing
+  // const [showLoading, setShowLoading] = useState(true)
+  // let timer1 = setTimeout(() => setShowLoading(true), 1000)
+  // useEffect(
+  //   () => {
+  //     // return () => {
+  //     //   clearTimeout(timer1)
+  //     // }
+  //     console.log("refreshing");
+  //   },
+  //   [showLoading]
+  // )
+
+  const [showLoading, setShowLoading] = useState(false)
+
+  useEffect(
+     () => {
+       let timer1 = setTimeout(() => setShowLoading(true), 1000)
+
+       // this will clear Timeout when component unmont like in willComponentUnmount
+      //  return () => {
+         console.log("refreshi8gn");
+      //  }
+     },
+     [showLoading]
+   )
+
+   setTimeout(() => {
+    console.log('Hello, World!')
+  }, 500);
+
 
   return (
     <>
@@ -78,107 +128,117 @@ export default function FriendsScreen({ navigation }) {
         containerStyle={{height: 'auto'}}
       />
       <ScrollView style={styles.gameListContainer}>
-        <Text style={ styles.titleStyle }>Friend request</Text>
+
+        <ButtonGroup
+            buttons={buttons}
+            containerStyle={styles.buttonGroup}
+            selectedIndex={screenState}
+            onPress={slider}
+          />
+        
         {
-          receivedRequests.length !== 0 ?
-            receivedRequests.map((person, index) => {
-              return (
-                <View style={styles.flexParent} key= {index}>
-                  <View style={styles.imageContainer}>
-                    <Avatar
-                      rounded
-                      size="large"
-                      source={{uri: person.avatar}}
-                    />
-                  </View>
-
-                  <View style={styles.textContainer}>
-                    <Text style = {styles.nameStyle}>
-                      {person.name}
-                    </Text>
-
-                    <Button
-                      buttonStyle={styles.button}
-                      title={"Confirm "}
-                      type='outline'
-                      iconRight={true}
-                      onPress={ 
-                        () => confirmFriendRequest(person.id)
-                       }
-                      icon={
-                        <Icon
-                          size={20}
-                          name={'info-circle'}
-                          type='font-awesome'
-                          color='#bdbdbd'
-                        />
-                      }
-                    />
-                  </View>
+          screenState === 1 && receivedRequests.length !== 0 ?
+          receivedRequests.map((person, index) => {
+            return (
+              <View style={styles.flexParent} key= {index}>
+                <Text style={ styles.titleStyle }>Friend request</Text>
+                <View style={styles.imageContainer}>
+                  <Avatar
+                    rounded
+                    size="large"
+                    source={{uri: person.avatar}}
+                  />
                 </View>
-              );
-            })
-          : <Text>No Friend Requests</Text>
-        }
 
-        <Text style={ styles.titleStyle }>{friendsList.length} Friends</Text>
-        {
-          friendsList.length !== 0 ?
-            friendsList.map((person, index) => {
-              return (
-                <View style={styles.flexParent} key= {index}>
-                  <View style={styles.imageContainer}>
-                    <Avatar
-                      rounded
-                      size="large"
-                      source={{uri: person.avatar}}
-                    />
-                  </View>
+                <View style={styles.textContainer}>
+                  <Text style = {styles.nameStyle}>
+                    {person.name}
+                  </Text>
 
-                  <View style={styles.textContainer}>
-                    <Text style = {styles.nameStyle}>
-                      {person.name}
-                    </Text>
-
-                    <Button
-                      buttonStyle={styles.button}
-                      title={"More info "}
-                      type='outline'
-                      iconRight={true}
-                      onPress={ 
-                        () => navigation.navigate('UserMoreInfo', {
-                          user: person
-                        })
-                       }
-                      icon={
-                        <Icon
-                          size={20}
-                          name={'info-circle'}
-                          type='font-awesome'
-                          color='#bdbdbd'
-                        />
-                      }
-                    />
-                    <Button
-                      buttonStyle={styles.button}
-                      title={"Unfriend"}
-                      type='outline'
-                      iconRight={true}
-                      onPress={()=> ( unfriend(person.id) )}
-                      icon={
-                        <Icon
-                          name="minus-circle"
-                          type="font-awesome"
-                          size={20}
-                          color='#bdbdbd'
-                        />
-                      }
-                    />
-                  </View>
+                  <Button
+                    buttonStyle={styles.button}
+                    title={"Confirm "}
+                    type='outline'
+                    iconRight={true}
+                    onPress={ 
+                      () => confirmFriendRequest(person.id)
+                    }
+                    icon={
+                      <Icon
+                        size={20}
+                        name={'info-circle'}
+                        type='font-awesome'
+                        color='#bdbdbd'
+                      />
+                    }
+                  />
                 </View>
-              );
-            })
-          : <Text>No friend in your library</Text>
+              </View>
+            );
+          })
+        : 
+        <Text>No Friend Requests</Text>
+      }
+
+       
+      {
+        screenState === 0 && friendsList.length !== 0 ?
+        friendsList.map((person, index) => {
+          return (
+            <View style={styles.flexParent} key= {index}>
+              <Text style={ styles.titleStyle }>{friendsList.length} Friends</Text>
+              <View style={styles.imageContainer}>
+                <Avatar
+                  rounded
+                  size="large"
+                  source={{uri: person.avatar}}
+                />
+              </View>
+
+              <View style={styles.textContainer}>
+                <Text style = {styles.nameStyle}>
+                  {person.name}
+                </Text>
+
+                <Button
+                  buttonStyle={styles.button}
+                  title={"More info "}
+                  type='outline'
+                  iconRight={true}
+                  onPress={ 
+                    () => navigation.navigate('UserMoreInfo', {
+                      user: person
+                    })
+                    }
+                  icon={
+                    <Icon
+                      size={20}
+                      name={'info-circle'}
+                      type='font-awesome'
+                      color='#bdbdbd'
+                    />
+                  }
+                />
+                <Button
+                  buttonStyle={styles.button}
+                  title={"Unfriend"}
+                  type='outline'
+                  iconRight={true}
+                  onPress={()=> ( unfriend(person.id) )}
+                  icon={
+                    <Icon
+                      name="minus-circle"
+                      type="font-awesome"
+                      size={20}
+                      color='#bdbdbd'
+                    />
+                  }
+                />
+              </View>
+            </View>
+          );
+        })
+        : <Text>No friend in your library</Text>
         }
 
       </ScrollView>
