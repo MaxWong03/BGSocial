566670e13db3f56f89dd1430e56b8f37dc0cd18e
MaxWong03 <madmaxmmajournal@gmail.com>
2019-11-04 21:02:46 -0800

    read preset duration from edit event

--- screens/EditPlayScreen.js
+++ screens/EditPlayScreen.js
@@ -6,22 +6,26 @@ import RecordGame from '../components/RecordGame';
 import RecordTime from '../components/RecordTime';
 import RecordDuration from '../components/RecordDuration';
 import { formatDuration } from '../utils/formatDuration';
+import { deformatDuration } from '../utils/deformatDuration';
+import useDuration from '../hooks/useDuration';
 
 
 export default function EditPlayScreen() {
   const users = useNavigationParam('users');
   const play = useNavigationParam('play');
-  const { date: presetDate, duration, game_id, id: playID, playsUsers } = play;
   const game = useNavigationParam('game');
+  const { date: presetDate, duration, game_id, id: playID, playsUsers } = play;
+  const { hour: presetHour, minute: presetMinute, second: presetSecond } = deformatDuration(duration);
 
   //states
   const [date, setDate] = useState(new Date(presetDate));
-  console.log('users:', users);
-  console.log('play:', play);
-  console.log('game:', game);
+  const { hour, minute, second, changeHour, changeMinute, changeSecond } = useDuration(presetHour, presetMinute, presetSecond);
 
   const editScoreAction = () => {
     console.log('date:', date);
+    console.log('hour:', hour);
+    console.log('minute:', minute);
+    console.log('second:', second);
   }
 
   return (
@@ -29,6 +33,14 @@ export default function EditPlayScreen() {
       <RecordTime
         date={date}
         setDate={setDate}
+      />
+      <RecordDuration
+        hour={hour}
+        minute={minute}
+        second={second}
+        changeHour={changeHour}
+        changeMinute={changeMinute}
+        changeSecond={changeSecond}
       />
       <Button
         title={'Edit Score!'}
