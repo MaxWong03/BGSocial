127f9913739b583c6169c6381e2f5d04ecdcf24b
MaxWong03 <madmaxmmajournal@gmail.com>
2019-11-13 11:39:15 -0800

    write unit test for arrayToObject

--- utils/index.test.js
+++ utils/index.test.js
@@ -1,5 +1,7 @@
 import { formatDateWithTime, formatTime, arrayToObject, getEventChosenEventDate, getEventMainImage, getConfirmedAttendants } from './index';
 
+const mockArray = [{ "id": 1 }, { "id": 3 }, { "id": 8 }];
+
 describe('formatDateWithTime', () => {
   test('format date object to day, month, date, at hour:minute AM/PM format', () => {
     const date = new Date('December 17, 1995 03:24:00');
@@ -27,5 +29,22 @@ describe('formatTime', () => {
   test('returns 3h when input is 03:00', () => {
     const formattedTime = formatTime('03:00');
     expect(formattedTime).toEqual('3h');
-  })
+  });
 });
+
+describe('arrayToObject', () => {
+
+  const obj = arrayToObject(mockArray, 'id');
+  const objKeysArr = Object.keys(obj);
+
+  test('returns an object of length 3', () => {
+    expect(objKeysArr.length).toEqual(3);
+  });
+
+  test('returns an object with the inner object id as key', () => {
+    expect(objKeysArr[0]).toEqual('1');
+    expect(objKeysArr[1]).toEqual('3');
+    expect(objKeysArr[2]).toEqual('8');
+  });
+});
+
